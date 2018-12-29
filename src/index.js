1f6bebf987d1fb1086e81405c0d72423b79f0378
Luis Alcaras <luisalcarasr@gmail.com>
2018-12-29 17:18:41 -0600

    The component was built (#3)
    * The component was created.
    
    * The component was cleaned.
    
    * The package file was updated.
    
    * The packages-lock.json was updated.
    
    * The component was built.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Index from './Index.vue';
+
+export function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component('IndexTable', Index);
+}
+
+const plugin = {
+    install,
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+export default Index;
