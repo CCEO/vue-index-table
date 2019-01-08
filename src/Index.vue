fe78002b3386b0d6771b51b8a27c67bf28674772
Luis Alcaras <luisalcarasr@gmail.com>
2019-01-08 15:07:56 -0600

    Table options.

--- src/Index.vue
+++ src/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="dynamic-tables">
-        <b-breadcrumb>
+        <b-breadcrumb v-if="typeof breadcrumbs !== 'undefined'">
             <b-breadcrumb-item v-for="breadcrumb in breadcrumbs">
                 <a :href="breadcrumb.url">
                     <i :class="breadcrumb.icon"></i>{{ breadcrumb.icon ? " " : "" }}{{ breadcrumb.text }}
@@ -8,20 +8,20 @@
             </b-breadcrumb-item>
         </b-breadcrumb>
         <h2 class="page-title">{{ title }}</h2>
-        <b-card :title="subtitle" collapse close customHeader>
+        <b-card :title="subtitle" :class=" hasCard ? '' : 'no-card' " collapse close customHeader>
             <b-button-group class="pull-right float-right">
                 <b-button v-for="button in toolbar" :variant="button.variant"
-                          class="btn-rounded-f width-100 mb-xs mr-xs" :href="button.url"
-                          @click="button.tap">
+                          class="btn-rounded-f width-100 mb-xs mr-xs" :href="button.url" @click="button.tap">
                     <i v-if="button.icon" :class="button.icon"></i>{{ button.icon ? " " : "" }}{{ button.text }}
                 </b-button>
             </b-button-group>
-            <v-client-table :data="data" :columns="columns" :options="options">
-                <div slot="actions" slot-scope="props">
+            <v-client-table :data="data" :columns="columns" :options="settings">
+                <div :slot="actionsColumn" slot-scope="props">
                     <b-button-group size="sm">
                         <b-button class="btn-rounded-f pull-right" v-for="button in buttons"
-                                  v-b-modal="button.name" :variant="button.variant" :href="button.URL"
-                                  @click="showModal(button)">
+                                  v-if="button.exceptions ? button.exceptions.indexOf(props.row.id) < 0 : true"
+                                  v-b-modal="button.name" :variant="button.variant"
+                                  :href="button.url.replace(':id', props.row.id)" @click="showModal(button)">
                             <i :class="button.icon"></i>
                         </b-button>
                     </b-button-group>
@@ -44,20 +44,36 @@
             toolbar: {type: Array},
             data: {type: Array},
             columns: {type: Array},
-            buttons: {type: Array}
+            actionsColumn: {type: String},
+            buttons: {type: Array},
+            hasCard: {type: Boolean},
+            options: {type: Object}
         },
         data() {
-            return {
-                options: {
-                    perPage: 10,
-                    pagination: {chunk: 10, dropdown: false},
-                    texts: {filter: '', count: '', limit: ''},
-                    columnsClasses: {id: 'width-100'},
-                    skin: 'table table-striped',
-                    sortIcon: {
-                        base: 'fa text-muted', up: 'fa-chevron-up', down: 'fa-chevron-down', is: 'fa-sort',
-                    }
+            let defaultOptions = {
+                perPage: 5,
+                pagination: {
+                    chunk: 5,
+                    dropdown: false,
                 },
+                texts: {
+                    filter: '',
+                    count: '',
+                    limit: ''
+                },
+                //skin: 'table table-striped',
+                sortIcon: {
+                    base: 'fa text-muted ml-1',
+                    up: 'fa-chevron-up ml-1',
+                    down: 'fa-chevron-down ml-1',
+                    is: 'fa-sort ml-1',
+                }
+            };
+            let options = this.options ? Object.assign(this.options, defaultOptions) : defaultOptions;
+
+            console.log(options.pagination);
+            return {
+                settings: options,
                 modal: {
                     variant: 'primary',
                     title: '',
@@ -85,3 +101,10 @@
         }
     }
 </script>
+
+<style lang="css" scoped>
+    .no-card {
+        border: 0;
+        background-color: transparent;
+    }
+</style>
